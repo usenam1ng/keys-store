fe7bd4698c5362ed29e4e75280152e119cd6351f
Egor Sderzhikov <dddeflix@gmail.com>
2024-05-16 00:13:33 +0700

    referral

--- server/database container/init.sql
+++ server/database container/init.sql
@@ -1,9 +1,14 @@
+-- Создание таблицы пользователей
 CREATE TABLE users (
     chat_id INT8 PRIMARY KEY,
-    user_id INT8 NOT NULL,
-    bought_id INT NOT NULL
+    user_id INT8 NOT NULL UNIQUE,
+    bought_id INT NOT NULL,
+    referrer_id INT8,
+    discount INT DEFAULT 0,
+    FOREIGN KEY (referrer_id) REFERENCES users(user_id)
 );
 
+-- Создание таблицы аккаунтов
 CREATE TABLE accounts (
     acc_email TEXT PRIMARY KEY,
     acc_password TEXT NOT NULL,
@@ -12,15 +17,23 @@ CREATE TABLE accounts (
     users_list JSONB
 );
 
+-- Создание таблицы игровых ключей
 CREATE TABLE keys (
     game_key VARCHAR(64) NOT NULL,
     game_name TEXT NOT NULL,
     platform TEXT NOT NULL
 );
 
+-- Создание таблицы технической поддержки
 CREATE TABLE tech_support (
     admin_username TEXT NOT NULL,
     admin_chat_id INT8 NOT NULL, 
     hit_count INT NOT NULL
 );
 
+-- Создание таблицы реферальных связей
+CREATE TABLE referrals (
+    user_id INT8 REFERENCES users(user_id),
+    referred_user_id INT8 UNIQUE REFERENCES users(user_id),
+    PRIMARY KEY (user_id, referred_user_id)
+);
--- server/server container/main.py
+++ server/server container/main.py
@@ -8,12 +8,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# bot = telebot.TeleBot("5000035098:AAFzHkyiU8Fso5QUlSbcWfGMJyAh2QB3ZnY/test")
 bot = telebot.TeleBot("5000224649:AAGmCkeaxsD7ZCzPnYavep7rsOY-V9BoSsc/test")
 databaseConnection = psycopg2.connect(dbname="app", user='app_user', password="jaeQuu7ziweeci5e", host="db", port="6666")
 databaseCursor = databaseConnection.cursor()
 
-def registerUser(message):
+def register_user(message, referral_id=None):
     cursor = databaseConnection.cursor()
     select_statement = """
         SELECT * FROM users
@@ -24,31 +23,33 @@ def registerUser(message):
 
     if len(result) == 0:
         try:
-            insert_statement = """
-            INSERT INTO users (chat_id, user_id, bought_id)
-            VALUES (%s, %s, %s)"""
+            if referral_id:
+                # Обрабатываем реферальный случай
+                databaseCursor.execute("UPDATE users SET discount = 10 WHERE user_id = %s", (referral_id,))
 
-            # Execute the INSERT statement with the provided data
-            databaseCursor.execute(insert_statement, (message.chat.id, message.from_user.id, 0))
+                discount = 5
+                insert_statement = """
+                    INSERT INTO users (chat_id, user_id, bought_id, referrer_id, discount)
+                    VALUES (%s, %s, %s, %s, %s)"""
+                databaseCursor.execute(insert_statement, (message.chat.id, message.from_user.id, 0, referral_id, discount))
 
-            # Commit the changes to the database
+                # Добавление в таблицу referrals
+                databaseCursor.execute("INSERT INTO referrals (user_id, referred_user_id) VALUES (%s, %s)", (referral_id, message.from_user.id))
+            else:
+                insert_statement = """
+                INSERT INTO users (chat_id, user_id, bought_id)
+                VALUES (%s, %s, %s)"""
+                databaseCursor.execute(insert_statement, (message.chat.id, message.from_user.id, 0))
+
             databaseConnection.commit()
-
             print("registered user")
         except (Exception, psycopg2.DatabaseError) as e:
             bot.send_message(message.chat.id, str(e))
     else:
         bot.send_message(message.chat.id, "Добро пожаловать, снова.")
 
-
 @bot.message_handler(commands=['iamadminnow_SUPERSECRETPASSWORD'])
-def addAdmin(message):
-    # CREATE TABLE tech_support (
-    #     admin_username TEXT NOT NULL,
-    #     admin_chat_id INT8 NOT NULL, 
-    #     hit_count INT NOT NULL
-    # );
-
+def add_admin(message):
     cursor = databaseConnection.cursor()
     select_statement = """
         SELECT * FROM tech_support
@@ -63,10 +64,7 @@ def addAdmin(message):
             INSERT INTO tech_support (admin_username, admin_chat_id, hit_count)
             VALUES (%s, %s, %s)"""
 
-            # Execute the INSERT statement with the provided data
             databaseCursor.execute(insert_statement, (message.from_user.username, message.chat.id, 0))
-
-            # Commit the changes to the database
             databaseConnection.commit()
 
             print("added admin ", (message.from_user.username, message.chat.id, 0))
@@ -76,41 +74,36 @@ def addAdmin(message):
     else:
         bot.send_message(message.chat.id, "Вы уже администратор.")
 
-# Обработчик /start
+# Обработчик /start с поддержкой реферальных ссылок
 @bot.message_handler(commands=["start"])
-def echo_all(message):
-    # Создание клавиатуры с кнопками
+def handle_start(message):
+    referral_id = None
+    if len(message.text.split()) > 1:
+        referral_id = message.text.split()[1]
+
+    if referral_id:
+        register_user(message, referral_id=referral_id)
+    else:
+        register_user(message)
+
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     btn1 = telebot.types.KeyboardButton("Отзывы")
     btn2 = telebot.types.KeyboardButton("Тех-поддержка")
     btn3 = telebot.types.KeyboardButton("Рефералочки")
     keyboard.add(btn1).row(btn2, btn3)
 
-    # Отправка сообщения с клавиатурой
-    bot.send_message(message.chat.id, "Привет! Добро пожаловать в наш маленький уютный магазинчик. Доступ к сайту осуществляется по кнопке *Магазин*. Ну и снизу красивое меню тоже, потыкай", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Привет! Добро пожаловать в наш маленький уютный магазинчик.", reply_markup=keyboard)
 
-    registerUser(message);
-
-
-# Обработчик /_get_chat_id
 @bot.message_handler(commands=["getchatid"])
 def get_chat_id(message):
     bot.send_message(message.chat.id, str(message.chat.id))
 
-
-# Обработчик текстовых сообщений
 @bot.message_handler(content_types=['text'])
-def textMessageHandlers(message):
-    # remove users message (thats pretty)
+def text_message_handlers(message):
     bot.delete_message(message.chat.id, message.id)
 
     if message.text == "Тех-поддержка":
-        # make a request to base
-        # find admin with less hit count
-        # redirect to him
-        
         try:
-            # Get admin with less hit_count
             databaseCursor.execute("SELECT admin_username FROM tech_support ORDER BY hit_count ASC LIMIT 1;")
             username = databaseCursor.fetchone()
             databaseCursor.execute("""UPDATE tech_support
@@ -127,19 +120,18 @@ def textMessageHandlers(message):
             bot.send_message(message.chat.id, str(e))
     elif message.text == "Отзывы":
         bot.send_message(message.chat.id, "Отзывы в канале: t.me/ggstore_community/2")
+    elif message.text == "Рефералочки":
+        referral_link = f"https://t.me/your_bot_username?start={message.from_user.id}"
+        bot.send_message(message.chat.id, f"Ваша реферальная ссылка: {referral_link}")
 
-
-# Обработчик сообщений из webApp
 @bot.message_handler(content_types="web_app_data") 
-def webAppHandler(message):
-   print(message) 
-   print(message.web_app_data.data) 
-   bot.send_message("5000971271", str(message.web_app_data.data))
+def web_app_handler(message):
+    print(message) 
+    print(message.web_app_data.data) 
+    bot.send_message("5000971271", str(message.web_app_data.data))
 
 @app.route('/request', methods=['POST'])
 def request_handler():
-    print("RUNNING")
-    
     data = request.get_json()
     buyType = data.get('type')
     user_id = data.get('user_id')
@@ -155,18 +147,12 @@ def request_handler():
             WHERE user_id = %s
         """
         databaseCursor.execute(select_statement, (user_id,))
-
-        # Fetch the result
         result = databaseCursor.fetchone()
 
-        # Close the cursor and the connection
-        
-        # Return the chat_id if the user was found, otherwise return None
         chat_id = result[0]
 
-        bot.send_message(chat_id, "BUTTON PUSH!!!!!:");
-        bot.send_message(chat_id, str(data));
-        incomingBuy(chat_id, data)
+        bot.send_message(chat_id, "BUTTON PUSH!!!!!:")
+        bot.send_message(chat_id, str(data))
 
         return jsonify({'success': 'Request processed'}), 200
 
@@ -174,32 +160,15 @@ def request_handler():
         print("Error getting user's chat_id: ", error)
         return None
 
-
-
-def incomingBuy(chat_id, data):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    inlinebutton = telebot.types.InlineKeyboardButton(text="🔥 Оплатить 🔥", callback_data="https://google.ru")
-    keyboard.add(inlinebutton)
-    bot.send_message(chat_id, inlinebutton, "")
-
-    bot.send_message(chat_id, "Оплата заказа доступна по кнопке под сообщением.", reply_markup=keyboard)
-
-
-
-
-
-# Запуск бота
-def startBot():
+def start_bot():
     bot.polling()
 
-def flaskApiServer():
-    print("RUNNING")
+def flask_api_server():
     app.run(host='0.0.0.0', port=9999)
 
 if __name__ == "__main__":
-    
-    t1 = threading.Thread(target=startBot)
-    t2 = threading.Thread(target=flaskApiServer)
+    t1 = threading.Thread(target=start_bot)
+    t2 = threading.Thread(target=flask_api_server)
 
     t1.start()
     t2.start()
